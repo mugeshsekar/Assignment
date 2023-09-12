d7691dee339cdd4e2ddd23e5231fd2b5726d6af7
mugeshsekar <144760753+mugeshsekar@users.noreply.github.com>
2023-09-12 10:56:02 +0530

    Add files via upload

---
+++ 11-09/mark.py
@@ -0,0 +1,13 @@
+name = " Mugesh"
+address= "salem"
+doing="M.sc (computer science)"
+tamil=80
+english=99
+maths=60
+physics=80
+chemistry=70
+computerscience=98
+total= tamil+english+maths+physics+chemistry+computerscience
+avarage=total/600*100
+print("Name:", name, "Adress:" , address , "Doing:" , doing ,"Mark:" , tamil,english,physics,maths,chemistry,computerscience,"Total:",total,"Avarage:",avarage)
+
